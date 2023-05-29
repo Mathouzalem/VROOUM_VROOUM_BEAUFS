5411960f45952ea6ff7fb2e9effd77dd82be2530
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-05-29 18:00:40 +0200

    feat: new&create cars method done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,21 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:title, :photo, :description, :price, :motor, :fuel, :brand, :year, :modele, :power)
+  end
 end
