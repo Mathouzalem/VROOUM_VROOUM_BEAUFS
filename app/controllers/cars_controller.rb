941a19f95a18025dd5e2d40b2262735113b43f47
Mathieu Cozian <mathieu.cozian@ieseg.fr>
2023-05-29 18:03:59 +0200

    Started the design

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,8 +5,11 @@ class CarsController < ApplicationController
 
   def update
     @car = Car.find(params[:id])
-    @car.update(car_params)
-    redirect_to car_path(@car)
+    if @car.update(car_params)
+      redirect_to car_path(@car)
+    else
+      render :edit, stauts: :unprocessable_entity
+    end
   end
 
   private
