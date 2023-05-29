3cff928de76d47bcc5463760ea3791993e14612f
Mathieu Cozian <mathieu.cozian@ieseg.fr>
2023-05-29 16:29:52 +0200

    Made the controller (edit, update, params)

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,17 @@
 class CarsController < ApplicationController
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:title, :photo, :description, :price, :motor, :fuel, :brand, :year, :modele, :power)
+  end
 end
