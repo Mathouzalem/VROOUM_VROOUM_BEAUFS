cfa731dd99db551567b8be70d51807b8ecd79d21
Mathieu Cozian <mathieu.cozian@ieseg.fr>
2023-05-31 16:51:37 +0200

    Feature works but no front

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,4 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_143357) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_140908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_143357) do
     t.datetime "updated_at", null: false
     t.date "start_date"
     t.date "end_date"
+    t.bigint "review_id"
     t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["review_id"], name: "index_bookings_on_review_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
