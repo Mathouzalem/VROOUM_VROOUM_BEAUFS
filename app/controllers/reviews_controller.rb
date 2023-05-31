37977f6e9bbeb3ffeb1013372e0440c51e325686
Mathieu Cozian <mathieu.cozian@ieseg.fr>
2023-05-31 15:29:16 +0200

    Done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,30 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.where(booking_id: params[:booking_id])
+  end
+
+  def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking_id = @booking.id
+    @car = @review.booking.car.id
+    if @review.save
+      redirect_to car_path(@car)
+    else
+      render :new, :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rate)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
+  validates_length_of :comment, :minimum => 5
   belongs_to :booking
 end
